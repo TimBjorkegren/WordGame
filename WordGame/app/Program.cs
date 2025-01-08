ee1261486ffeb1e77e76178a07336811e7d9bf7c
Danne <daniel.theoren@gmail.com>
2025-01-08 11:58:25 +0100

    Cookie creater
    added dependencies to run app.use func that creates a new cookie

--- WordGame/app/Program.cs
+++ WordGame/app/Program.cs
@@ -1,21 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
 using System.Security.Cryptography;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 
 string clientId;
-HttpContent context;
-var app = WebApplication.CreateBuilder(args);
+//HttpContext context;
 
-public void BakeCookie()
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+//void BakeCookie()
+app.Use(async (context, next) =>
 {
     clientId = GenerateUniqueClientId();
-    context.Response.C
-}
+    context.Response.Cookies.Append("ClientID", clientId, new CookieOptions
+    {
+        HttpOnly = true,
+        Secure = false,
+        SameSite = SameSiteMode.Strict,
+        MaxAge = TimeSpan.MaxValue
+    });
+    await next();
+});
+    
 
 static string GenerateUniqueClientId()
-{
-    var rng = RandomNumberGenerator.Create();
-    var id = new byte[16];
-    rng.GetBytes(id);
-    return Convert.ToBase64String(id);
-}
+    {
+        var rng = RandomNumberGenerator.Create();
+        var id = new byte[16];
+        rng.GetBytes(id);
+        return Convert.ToBase64String(id);
+    }
