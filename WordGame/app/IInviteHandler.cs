cbb143c97e2f412b772dd2b2ab2869834991c65f
Tim <tim.bjorkegren@gmail.com>
2025-01-08 15:05:24 +0100

    ShuffleString Algorithm
    Shuffle algorithm made so the Lobby Codes will be shuffled, made this
    because its easier for everyone to understand

--- WordGame/app/IInviteHandler.cs
+++ WordGame/app/IInviteHandler.cs
@@ -8,6 +8,20 @@ public IInviteHandler GenerateInvite()
 
 private string GenerateLobbyCode(){
 
+    var random = new Random();
+    const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    return new string Collections.shuffle(chars);
+}
+
+public static string ShuffleString(string chars, int length){
+
+    random = new Random();
+
+    return new string(
+        chars.OrderBy(c => random.Next())
+        .Take(length)
+        .ToArray()
+    )
 }
 
 private void SaveLobbyCodeToDatabase(){
