35887138b2f6ee0d31bfd70648f31ce547be9124
PATRIK DA DON <patrik.goransson55@gmail.com>
2025-01-16 16:04:40 +0100

    nothing

--- app/obj/Debug/net8.0/app.AssemblyInfo.cs
+++ app/obj/Debug/net8.0/app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+569c5a90d08f98df88e3f07ea549f63847098d72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c41b0cf447c4ad1fcc5ca6da7cf4f72d73d73c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
